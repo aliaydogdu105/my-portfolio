f8ad94260f533fd28a3254c4fdbc5b378c5bca26
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-10-15 04:14:02 +0300

    home  v2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="d-flex justify-content-center p-5 vh-100 bg-light">
-      <h1>
-        Hi, I'm Ali. I’m currently interested HTML, CSS, JavaScript, React.
+    <div className="mainPage d-flex align-items-center flex-column vh-100 pt-5">
+      <h1 className="mt-5 pt-5">
+        <strong>Hi, I'm Ali</strong>
       </h1>
+      <p className="w-50 mt-5">
+        I'm a Software Developer. I’m currently interested front-end
+        development; React. My plan is to learn Django and Nodejs for the
+        back-end. Ask me any questions you have about front-end development. I'd
+        be glad to help you...
+      </p>
     </div>
   );
 };
