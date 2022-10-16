fccc5d7c194e640fdb19130d03775c2394360329
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-10-16 23:59:20 +0300

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { FaLinkedin, FaRegEnvelope, FaGithub } from "react-icons/fa";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact d-flex justify-content-center align-items-center vh-100">
+      <div>
+        <a
+          href="https://www.linkedin.com/in/aliaydogdu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={50} />
+        </a>
+      </div>
+      <div>
+        <a
+          href="mailto:aliaydogdu105@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaRegEnvelope size={50} />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://github.com/aliaydogdu105"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={50} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
