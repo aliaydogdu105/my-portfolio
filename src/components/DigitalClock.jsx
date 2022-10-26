625cedf6afc971c5a9571266357aab7df0e32e6f
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-10-26 23:59:39 +0300

    time and date

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -3,6 +3,16 @@ import { useState, useEffect } from "react";
 
 const DigitalClock = () => {
   const [time, setTime] = useState(new Date());
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayName = days[time.getDay()];
   useEffect(() => {
     let timeId = setInterval(() => setTime(new Date()), 1000);
     return () => {
@@ -10,7 +20,32 @@ const DigitalClock = () => {
     };
   });
 
-  return <div style={{ color: "white" }}>{time.toLocaleTimeString()} </div>;
+  return (
+    <div
+      style={{ color: "white", fontFamily: "monospace", fontWeight: "lighter" }}
+    >
+      {dayName}
+      <hr />
+      {time.toLocaleTimeString()}
+    </div>
+  );
+};
+export const Day = () => {
+  let today = new Date();
+  return <div style={{ color: "yellow" }}>{today} </div>;
 };
 
 export default DigitalClock;
+
+// export default function DigitalClock() {
+//   const current = new Date();
+//   const date = `${current.getDate()}/${
+//     current.getMonth() + 1
+//   }/${current.getFullYear()}-${current.getHours()}-${current.getMinutes()}-${current.getSeconds()}`;
+
+//   return (
+//     <div className="App text-bg-light">
+//       <h1>Current date is {date}</h1>
+//     </div>
+//   );
+// }
