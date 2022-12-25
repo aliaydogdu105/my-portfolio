8e9cb7537f2616be3e1b98890a755d323a749592
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-12-25 18:39:19 +0300

    feat: added certificate

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,10 +3,13 @@ import { data } from "../helpers/data";
 
 const Projects = () => {
   return (
-    <div className="projects d-flex justify-content-center align-items-center flex-wrap">
-      {data.map((projectcards) => {
-        return <ProjectCards key={projectcards.id} {...projectcards} />;
-      })}
+    <div className="about d-flex justify-content-center align-items-center flex-column pt-2">
+      <h1>Projects</h1>
+      <div className="projects d-flex justify-content-center align-items-center flex-wrap">
+        {data.map((projectcards) => {
+          return <ProjectCards key={projectcards.id} {...projectcards} />;
+        })}
+      </div>
     </div>
   );
 };
